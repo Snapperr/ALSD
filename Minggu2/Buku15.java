6a644616eb419f2362b44118f78fff692b3b30b0
Snapperr <mikaila082@gmail.com>
2024-02-23 14:53:33 +0700

    stok lebih besar 0

--- Minggu2/Buku15.java
+++ Minggu2/Buku15.java
@@ -1,1 +1,24 @@
-a
+public class Buku15 {
+        String judul, pengarang;
+        int harga, stok, halaman;
+
+        void tampilinformasi(){
+            System.out.println("Judul: "+ judul);
+            System.out.println("pengaran: "+ pengarang);
+            System.out.println("Jumlah Halaman: "+ halaman);
+            System.out.println("Sisa Stok: "+ stok);
+            System.out.println("Harga: "+ harga);
+        }
+
+        void terjual(int jml){
+            if (stok > 0) {
+            stok -= jml;
+            }
+        }
+        void restock(int jml){
+            stok += jml;
+        }
+        void gantiharga(int hrg){
+            harga = hrg;
+        }
+    }
